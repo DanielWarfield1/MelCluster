6d4f1c18be9ab2eb61dc78b4a2170424ee9c73e6
Daniel Warfield <danielwarfield1@gmail.com>
2021-04-20 01:06:57 -0400

    added sleep to the test

--- MelCluster/test/npmShellTest.py
+++ MelCluster/test/npmShellTest.py
@@ -1,5 +1,6 @@
 import sys
 import MelCluster
+import time
 
 #should accept data from https://www.npmjs.com/package/python-shell
 #prints the previously input data
@@ -9,6 +10,9 @@ def main():
 		newData = sys.stdin.readline()
 		print(data)
 		data = newData
+
+		#change this to change delay time
+		time.sleep(3)
 
 		if newData == '-1':
 			break
